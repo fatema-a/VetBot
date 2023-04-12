26c84e7f5ea0c3f5f06c014ff1ea9bd4e5d62aaf
Aykchen <aliceykchen01@gmail.com>
2023-04-11 23:18:48 -0400

    updated welcome page

--- app/src/home.jsx
+++ app/src/home.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import { useState } from "react";
+import "./Home.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { Button } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import msgImg from "./conversation_chat_bubble_bubbles.png";
 
+function Home() {
+  return (
+    <Container id="container">
+      <Row id="msgImage">
+        <Image src={msgImg} />
+      </Row>
+      <Row id="Title">
+        <div>Welcome to VetBot</div>
+      </Row>
+      <Row id="Content">
+        <div>
+          Chat with an <br />
+          artificial intelligent <br />
+          veterinarian
+        </div>
+      </Row>
+      <Row>
+        <Button id="startButton">Start Chat</Button>
+      </Row>
+    </Container>
+  );
+}
 
-function home() {
-   
-    return (
-      <div></div>
-    )
-  }
-  
-  export default home
+export default Home;
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import Home from "./Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/chatbot" exact element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
