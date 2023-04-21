07db10dc45362c6031bcf07ab3a7aba6a1982586
fatema-a <fatema_n_a@hotmail.com>
2023-04-21 08:43:42 -0400

    adding newest key
    adding api key

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -26,7 +26,7 @@ import {
 import { Container, Row, Col } from "react-bootstrap";
 import vet from "./VetBot_logo.png";
 
-const API_KEY = "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";
+const API_KEY = "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";
 const systemMessage = {
   role: "system",
   content:
